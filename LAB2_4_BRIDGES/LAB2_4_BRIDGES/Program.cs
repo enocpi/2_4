63fb48160cdfbe61b831f7405ce22e4a757a414f
enocpi <61128286+enocpi@users.noreply.github.com>
2020-03-10 13:05:24 -0500

    Add files via upload

---
+++ LAB2_4_BRIDGES/LAB2_4_BRIDGES/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LAB2_4_BRIDGES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myFirstNumber = 19;
+            int mySecondNumber = 244 * (int)myFirstNumber;
+            Console.WriteLine(mySecondNumber);
+            string myText = "Some Text";
+            Console.WriteLine(mySecondNumber + myText);
+            string moreText = "goes here";
+            Console.WriteLine(myText + moreText);
+            string color1 = "yellow";
+            string color2 = "red";
+            const bool isYellow = true;
+            const bool isGreen = false;
+            Console.WriteLine(color1 + isYellow); 
+                Console.WriteLine( color2 + isGreen);
+            double numbers;
+
+            numbers = mySecondNumber / myFirstNumber;
+            Console.WriteLine(numbers);
+
+            Console.WriteLine("Breakpoint Here");
+
+            string textToNumber = "80473";
+            int results = 99 * int.Parse(textToNumber);
+            Console.WriteLine(results);
+            DateTime rightNow = DateTime.Now;
+            Console.WriteLine(rightNow);
+
+
+
+
+
+        
+
+            
+          
+        }
+    }
+}
